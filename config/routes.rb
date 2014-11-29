97687b8fc60db8c726f416c0752b41dab99ed588
Sophie Drake <sophiedrake351@gmail.com>
2014-11-29 14:48:30 +0000

    Created Pages Info Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :ideas
+  get 'pages/info'
 
+  resources :ideas
+  root :to => redirect('/ideas')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
